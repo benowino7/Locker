bbec563720a615d46d6bb21683ec3f3a6167a0c7
benson7-coder <bensonowino7@gmail.com>
2020-01-28 08:40:20 +0300

    removed encription for server connectivity

--- ClearScreen.py
+++
@@ -1,9 +0,0 @@
-import os
-import platform
-import subprocess as sp
-
-def clearscrn():
-    if platform.system() == "'Linux'" or "'OSX'":
-        sp.call('clear',shell=True)
-    elif platform.system() == "'Windows'":
-        sp.call('cls',shell=True)
